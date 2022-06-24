f62d39376a5997a36d989933d51c684a8ca2183c
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-06-24 11:15:25 -0400

    numbers to objects

--- intermediat-challenges.js
+++ intermediat-challenges.js
@@ -1,10 +1,22 @@
+// numbers to objects
+function numObj(s){
+    let arr = [];
+    for(let i = 0; i < s.length; i++) {
+        arr[i] = {};
+        arr[i][s[i].toString()] = String.fromCharCode(s[i]);
+    }
+    return arr;
+}
+
+console.log(numObj([101,121,110,113,113,103]))
+
 
 // Magic Index
-function findMagic(arr){
-  for(let i = 0; i < arr.length; i++) {
-    if(arr[i] === i) {
-        return i;
-    }
-  }
-  return -1;
-}
+// function findMagic(arr){
+//   for(let i = 0; i < arr.length; i++) {
+//     if(arr[i] === i) {
+//         return i;
+//     }
+//   }
+//   return -1;
+// }
