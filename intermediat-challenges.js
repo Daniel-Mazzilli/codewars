8515a181706e8fba7ace1b250b91e3dffca04218
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-06-26 20:12:33 -0400

    well of ideas - harder ver

--- intermediat-challenges.js
+++ intermediat-challenges.js
@@ -1,14 +1,31 @@
-// numbers to objects
-function numObj(s){
-    let arr = [];
-    for(let i = 0; i < s.length; i++) {
-        arr[i] = {};
-        arr[i][s[i].toString()] = String.fromCharCode(s[i]);
+// well of ideas - sorta hard coded a bit
+function well(x){
+    let goodCount = 0;
+    for(const arr of x) {
+      for(const item of arr) {
+        if(item[0] === `g` || item[0] === `G`) {
+          goodCount ++;
+        }
+      }
     }
-    return arr;
-}
+    if(goodCount > 2) {
+      return `I smell a series!`;
+    } else if (goodCount > 0) {
+      return `Publish!`;
+    }
+    return `Fail!`;
+  }
+// numbers to objects
+// function numObj(s){
+//     let arr = [];
+//     for(let i = 0; i < s.length; i++) {
+//         arr[i] = {};
+//         arr[i][s[i].toString()] = String.fromCharCode(s[i]);
+//     }
+//     return arr;
+// }
 
-console.log(numObj([101,121,110,113,113,103]))
+// console.log(numObj([101,121,110,113,113,103]))
 
 
 // Magic Index
