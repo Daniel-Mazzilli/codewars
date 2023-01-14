50ea03d354dde53d65420becfc2c29b5a6de1b6c
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-01-14 02:37:46 -0500

    duplicates everywhere

--- jan_2023.js
+++ jan_2023.js
@@ -1,3 +1,25 @@
+// Duplicates. Duplicates everywhere
+const table = {
+  1: ["A", "B", "C"],
+  2: ["A", "B", "D", "A"],
+  3: ["A", "B", "D", "A"],
+};
+
+const removeDuplicateIds = (obj) => {
+  for (const k in obj) {
+    obj[k] = [...new Set(obj[k])];
+    for (const i in obj) {
+      if (+k < +i) {
+        obj[k] = obj[k].filter((e) => !obj[i].includes(e));
+      }
+    }
+  }
+  return obj;
+};
+
+// console.log(removeDuplicateIds(table));
+
+// Combine Objects
 const objA = { a: 10, b: 20, c: 30 };
 const objB = { a: 3, c: 6, d: 3 };
 const objC = { a: 5, d: 11, e: 8 };
@@ -23,4 +45,4 @@ const combine = (...params) => {
   return sumVals;
 };
 
-console.log(combine(objA, objB, objC));
+// console.log(combine(objA, objB, objC));
